3ed87c1d8aa58866177117e3d7b183393b649cb2
shashank <shashankdwivedi500@gmail.com>
2021-02-07 15:14:42 -0800

    heroku log in

--- shashank/settings.py
+++ shashank/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 """
 Django settings for shashank project.
 
@@ -120,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+django_heroku.settings(locals())
